b59650094b8c57415eee0cd41c1972ac2ab39296
YongkyA23 <67529856+YongkyA23@users.noreply.github.com>
2023-06-15 16:18:55 +0700

    image slider

--- js/script.js
+++ js/script.js
@@ -1,3 +1,66 @@
+var $slider = $(".carousel");
+
+var $sliderContainer = $(".carousel-container");
+var $slides = $(".carousel-image");
+
+var $slideWidth = $slides.width();
+
+var $slideHeight = $slides.height();
+
+var $slideCount = $slides.length;
+
+var $slideIndex = 0;
+
+var $totalWidth = $slideCount * $slideWidth;
+
+$slider.css({ width: $slideWidth, height: $slideHeight });
+
+$sliderContainer.css({
+  width: $totalWidth,
+  height: $slideHeight,
+  marginLeft: -$slideWidth,
+});
+
+$slides.css({ width: $slideWidth, height: $slideHeight });
+
+function goToSlide(index) {
+  if (index < 0) {
+    index = $slideCount - 1;
+  } else if (index >= $slideCount) {
+    index = 0;
+  }
+
+  $sliderContainer.css({ marginLeft: index * -$slideWidth });
+
+  $slideIndex = index;
+}
+
+$("#left-arrow").click(function () {
+  $sliderContainer.animate(
+    {
+      left: +$slideWidth,
+    },
+    "slow",
+    () => {
+      $(".carousel-image:last-child").prependTo(".carousel-image-container");
+      $sliderContainer.css("left", "");
+    }
+  );
+});
+
+$("#right-arrow").click(function () {
+  $sliderContainer.animate(
+    {
+      left: -$slideWidth,
+    },
+    "slow",
+    () => {
+      $(".carousel-image:first-child").appendTo(".carousel-image-container");
+      $sliderContainer.css("left", "");
+    }
+  );
+});
+
 // Get the menu icon and navbar items elements
 const menuIcon = document.getElementById("menu-icon");
 const navbarItems = document.querySelector(".navbaritems");

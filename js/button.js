f5c4379df719533eb0078be4a235c30e16ccfce6
YongkyA23 <67529856+YongkyA23@users.noreply.github.com>
2023-06-16 23:39:43 +0700

    KELAR
    fel tolong di cek lgi y

--- js/button.js
+++ js/button.js
@@ -34,10 +34,3 @@ if (sessionStorage.getItem("isLoggedIn")) {
   registerButton.innerHTML = '<a href="register.html">Register</a>';
   loginButton.innerHTML = '<a href="login.html">Login</a>';
 }
-
-const menuToggle = document.querySelector(".menu-toggle");
-const mobileMenu = document.querySelector(".mobile-menu");
-
-menuToggle.addEventListener("click", () => {
-  mobileMenu.classList.toggle("active");
-});

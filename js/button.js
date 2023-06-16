cf63b8cf7f100fddbba14ebbfc3e7414716c53c2
YongkyA23 <67529856+YongkyA23@users.noreply.github.com>
2023-06-16 22:58:45 +0700

    responsif

---
+++ js/button.js
@@ -0,0 +1,43 @@
+const registerButton = document.querySelector(".register-button");
+const loginButton = document.querySelector(".login-button");
+const navbar = document.querySelector(".login-regis");
+
+if (sessionStorage.getItem("isLoggedIn")) {
+  // User is logged in
+  const userEmail = sessionStorage.getItem("userEmail");
+
+  // Create a new button with the user's email and dropdown menu
+  const buttonHTML = `
+    <div class="dropdown">
+      <button type="menu" class="nav-button">${userEmail}</button>
+      <div class="dropdown-content">
+        <a href="#" id="logout-button">Logout</a>
+      </div>
+    </div>
+  `;
+
+  // Replace the register and login buttons with the new button
+  navbar.innerHTML = buttonHTML;
+
+  // Add a click event listener to the logout button
+  const logoutButton = document.querySelector("#logout-button");
+  logoutButton.addEventListener("click", () => {
+    // Remove the isLoggedIn and userEmail keys from sessionStorage
+    sessionStorage.removeItem("isLoggedIn");
+    sessionStorage.removeItem("userEmail");
+
+    // Reload the page to update the buttons
+    window.location.reload();
+  });
+} else {
+  // User is not logged in
+  registerButton.innerHTML = '<a href="register.html">Register</a>';
+  loginButton.innerHTML = '<a href="login.html">Login</a>';
+}
+
+const menuToggle = document.querySelector(".menu-toggle");
+const mobileMenu = document.querySelector(".mobile-menu");
+
+menuToggle.addEventListener("click", () => {
+  mobileMenu.classList.toggle("active");
+});
